3d2b855eafbd08ad2336aaded16099899c72a462
hyrycham@gmail.com <hyrycham@gmail.com>
2021-08-20 00:49:52 +0300

    less2 hw

---
+++ lesson2/db/users.js
@@ -0,0 +1,15 @@
+ const users = [
+    {id:1, name: "Olya", gender: "female", age: 21, password: '1111'},
+    {id:2, name: "Valya", gender: "female", age: 22, password: '2222'},
+    {id:3, name: "Natasha", gender: "female", age: 23, password: '3333'},
+    {id:4, name: "Lena", gender: "female", age: 14, password: '4444'},
+    {id:5, name: "Ulya", gender: "female", age: 15, password: '5555'},
+    {id:6, name: "Vasya", gender: "male", age: 21, password: '6666'},
+    {id:7, name: "Petya", gender: "male", age: 22, password: '7777'},
+    {id:8, name: "Sasha", gender: "male", age: 23, password: '8888'},
+    {id:9, name: "Seryu", gender: "male", age: 14, password: '9999'},
+    {id:10, name: "Vitalik", gender: "male", age: 15, password: '0000'}
+];
+
+
+ module.exports=users;
